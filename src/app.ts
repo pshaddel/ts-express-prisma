de93ba5d2f38eb215e2f0fabbb03a0d214e42285
Poorshad <p.shaddel@gmail.com>
2020-09-11 18:05:54 +0430

    add request middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+
 const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use((req:express.Request, res:express.Response, next:express.NextFunction) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, AUTHORIZATION");
+	next();
+});
 
 app.get('/ping', (req: express.Request, res: express.Response) => {
       res.send('pong');
