1fdd7f932a0fa2d895a0694117ed5bac4b35093a
Poorshad <p.shaddel@gmail.com>
2021-04-20 23:56:55 +0430

    refactor: :recycle: Refactor project structure to better structure
    Remove body-parser package - refactor user functionality to service and controller files

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
-import Config from '../config';
-import createUser from './createUser';
-dotenv.config();
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import path from "path";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import Config from "../config";
+import { userRouter } from "./user/user.controller";
 
+dotenv.config();
 const config = Config();
 
 const app = express();
@@ -15,24 +14,23 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use((req:express.Request, res:express.Response, next:express.NextFunction) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, AUTHORIZATION");
-	next();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((_req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, AUTHORIZATION"
+  );
+  next();
 });
 
-app.get('/ping', (req: express.Request, res: express.Response) => {
-      res.send('pong');
+app.get("/ping", (_req: Request, res: Response) => {
+  res.send("pong");
 });
 
-app.post('/user', async (req: express.Request, res: express.Response) => {
-    const result = await createUser()
-	res.json({result});
-});
+app.post("/user", userRouter);
 
 app.listen(config.PORT);
-console.info("App is listenng on port:", config.PORT)
+console.info("App is listenng on port:", config.PORT);
---
+++ src/user/user.service.ts
@@ -0,0 +1,12 @@
+import { PrismaClient, User } from "@prisma/client";
+const prisma = new PrismaClient();
+export const createUser = async (user: {
+  name: string;
+  email: string;
+}): Promise<User> => {
+  const insertResult = await prisma.user.create({
+    data: user,
+  });
+  prisma.$disconnect();
+  return insertResult;
+};
